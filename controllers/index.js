2514ca8dcf1882a2bcc732b9060451e39de4dd48
carlos@milkzoft.com <Carlos.X.Santana.-ND@disney.com>
2014-09-08 20:06:15 -0700

    Translations fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var languages = global.config.application.languages;
 
-/* GET home page. */
-router.get('/', function(req, res) {
+router.get('/:lang(' + languages + ')', function(req, res) {
+	global.i18n.setLanguage(req.params.lang);
+	
   res.render('index', { welcome: global.lang.site.welcome });
 });
 
--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 module.exports = function(app) {
-  var languages = global.config.application.languages,
-      defaultController = require('./controllers/' + global.config.application.controllers.default);
+  var defaultController = require('./controllers/' + global.config.application.controllers.default);
 
   app.use('/', defaultController);
-  app.get('/:lang(' + languages + ')', defaultController);
 }
